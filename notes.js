af69f62ccfb88061eb069955ca99bdaebb91ff56
uditrpanchal <uditrpanchal@gmail.com>
2018-07-05 11:20:23 -0400

    Comment for method and section

--- notes.js
+++ notes.js
@@ -2,52 +2,66 @@ console.log('Starting notes.js');
 
 const fs = require('fs');
 
-//Fetch existing notes from notes-data.json and returns the JSON data object after converting from string.
+/*------------------- Fetch and Save methods------------------------------------ */
 var fetchNotes = () => {
-    try {
-        var noteString = fs.readFileSync('./playground/notes-data.json');
+	try {
+		var noteString = fs.readFileSync('./playground/notes-data.json');
 
-        return JSON.parse(noteString);
-    } catch (e) {
-        return [];
-    }
+		return JSON.parse(noteString);
+	} catch (e) {
+		return [];
+	}
 };
 
-
-//Saves the notes in string to notes-data.json
-var saveNotes = (notes) => {
-    fs.writeFileSync('./playground/notes-data.json', JSON.stringify(notes));
+var saveNotes = notes => {
+	fs.writeFileSync('./playground/notes-data.json', JSON.stringify(notes));
 };
+/*----------------------------------------------------------------------------- */
 
-
+/*-------------------ADD, REMOVE, READ, GET ALL NOTES------------------------- */
 var addNote = (title, body) => {
-    var notes = fetchNotes();
-    var note = {
-        title,
-        body,
-    };
+	var notes = fetchNotes();
+	var note = {
+		title:title,
+		body:body,
+	};
 
-    var duplicateNotes = notes.filter(note => {
-        return note.title === title;
-    });
+	var duplicateNotes = notes.filter(note => {
+		return note.title === title;
+	});
 
-    if (duplicateNotes.length === 0) {
-        notes.push(note);
-        saveNotes(notes);
-    }
+	if (duplicateNotes.length === 0) {
+		notes.push(note);
+		saveNotes(notes);
+	}
 };
 
 var getAll = () => {
-    console.log(fetchNotes());
+	console.log(fetchNotes());
+};
+
+var getNote = title => {
+	let allNotes = fetchNotes();
+	let readFlag = 0;
+	allNotes.forEach((data, index) => {
+		if (data.title === title) {
+			console.log(allNotes[index]);
+			readFlag = 1;
+		}
+	});
+
+	if (readFlag === 0) {
+		console.log(`${title} doesnot exist`);
+	}
 };
 
 var removeNote = title => {
-    let notes = fetchNotes();
-    let filteredNotes = notes.filter((note)=>{
-        return note.title !== title;
-    });
-    saveNotes(filteredNotes);
-}
+	let notes = fetchNotes();
+	let filteredNotes = notes.filter(note => {
+		return note.title !== title;
+	});
+	saveNotes(filteredNotes);
+};
 /*
 var removeNote = title => {
     let allNotes = fetchNotes();
@@ -63,25 +77,11 @@ var removeNote = title => {
     }
 };
 */
-
-var getNote = title => {
-    let allNotes = fetchNotes();
-    let readFlag = 0;
-    allNotes.forEach((data, index) => {
-        if (data.title === title) {
-            console.log(allNotes[index]);
-            readFlag = 1;
-        }
-    });
-
-    if (readFlag === 0) {
-        console.log(`${title} doesnot exist`);
-    }
-};
+/*----------------------------------------------------------------------------- */
 
 module.exports = {
-    addNote,
-    getAll,
-    removeNote,
-    getNote,
+	addNote,
+	getAll,
+	removeNote,
+	getNote,
 };
